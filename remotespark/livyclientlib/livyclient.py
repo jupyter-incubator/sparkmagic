25c32e6813f865c8e3904f6e67f516e3623bc37b
Alejandro Guerrero Gonzalez <alexgg@Alejandros-MacBook-Pro.local>
2015-09-25 03:02:26 -0700

    First crack at automatic pandas dataframes. Ugly two calls. Need to create sqlContext automatically for both pyspark and scala sessions. The more I think about it, the way to go is two wrapper kernels.

--- remotespark/livyclientlib/livyclient.py
+++ remotespark/livyclientlib/livyclient.py
@@ -11,6 +11,7 @@ class LivyClient(object):
     def __init__(self, spark_session, pyspark_session):
         self._spark_session = spark_session
         self._pyspark_session = pyspark_session
+        self._executed_sql = False
 
     def execute_scala(self, commands):
         self._spark_session.wait_for_state("idle")
@@ -21,15 +22,52 @@ class LivyClient(object):
         return self._pyspark_session.execute(commands)
 
     def execute_sql(self, command):
-        return self.execute_scala(self._make_sql(command))
+        if not self._executed_sql:
+            self.execute_pyspark(self._create_sql_context())
+            self._executed_sql = True
+
+        records_text = self.execute_pyspark(self._make_sql_collect(command))
+        columns_text = self.execute_pyspark(self._make_sql_columns(command))
+
+        records = eval(records_text)
+        columns = eval(columns_text)
+
+        import pandas as pd
+        return pd.DataFrame.from_records(records, columns=columns)
 
     def close_sessions(self):
         self._spark_session.delete()
         self._pyspark_session.delete()
 
-    def _make_sql(self, command):
-        sql = ""
-        sql += "val sqlContext = new org.apache.spark.sql.SQLContext(sc)\nimport sqlContext.implicits._\n"
-        sql += 'sqlContext.sql("' + command + '").collect()'
-
+    def _create_sql_context(self):
+        # TODO(aggftw): Create sqlContext by default on session start
+        sql = "from pyspark.sql import SQLContext\nfrom pyspark.sql.types import *\nsqlContext = SQLContext(sc)"
         return sql
+
+    def _make_sql_collect(self, command):
+        sql = 'sqlContext.sql("' + command + '").collect()'
+        return sql
+
+    def _make_sql_columns(self, command):
+        sql = 'sqlContext.sql("' + command + '").columns'
+        return sql
+
+
+class Row(tuple):
+    def __new__(cls, *args, **kwargs):
+        if args and kwargs:
+            raise ValueError("Can not use both args "
+                             "and kwargs to create Row")
+        if args:
+            # create row class or objects
+            return tuple.__new__(cls, args)
+
+        elif kwargs:
+            # create row objects
+            names = sorted(kwargs.keys())
+            row = tuple.__new__(cls, [kwargs[n] for n in names])
+            row.__fields__ = names
+            return row
+
+        else:
+            raise ValueError("No args or kwargs")
