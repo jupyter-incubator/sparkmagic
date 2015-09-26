a98c1abc99c80992332eb590aca8e9ee0a4f4f9a
Alejandro Guerrero Gonzalez <alexgg@Alejandros-MacBook-Pro.local>
2015-09-26 02:55:40 -0700

    Add sql context only once test

--- tests/test_livysession.py
+++ tests/test_livysession.py
@@ -6,7 +6,7 @@ from mock import MagicMock
 from remotespark.livyclientlib.livysession import LivySession
 
 
-class DummyResponse():
+class DummyResponse:
     def __init__(self, status_code, json_text):
         self._status_code = status_code
         self._json_text = json_text
@@ -22,11 +22,34 @@ class TestLivySession:
     pi_result = "Pi is roughly 3.14336"
 
     session_create_json = '{"id":0,"state":"starting","kind":"spark","log":[]}'
-    ready_sessions_json = '{"from":0,"total":1,"sessions":[{"id":0,"state":"idle","kind":"spark","log":["16:23:01,151 |-INFO in ch.qos.logback.core.joran.action.AppenderAction - Naming appender as [STDOUT]","16:23:01,213 |-INFO in ch.qos.logback.core.joran.action.NestedComplexPropertyIA - Assuming default type [ch.qos.logback.access.PatternLayoutEncoder] for [encoder] property","16:23:01,368 |-INFO in ch.qos.logback.core.joran.action.AppenderRefAction - Attaching appender named [STDOUT] to null","16:23:01,368 |-INFO in ch.qos.logback.access.joran.action.ConfigurationAction - End of configuration.","16:23:01,371 |-INFO in ch.qos.logback.access.joran.JoranConfigurator@53799e55 - Registering current configuration as safe fallback point","","15/09/04 16:23:01 INFO server.ServerConnector: Started ServerConnector@388859e4{HTTP/1.1}{0.0.0.0:37394}","15/09/04 16:23:01 INFO server.Server: Started @27514ms","15/09/04 16:23:01 INFO livy.WebServer: Starting server on 37394","Starting livy-repl on http://10.0.0.11:37394"]}]}'
-    busy_sessions_json = '{"from":0,"total":1,"sessions":[{"id":0,"state":"busy","kind":"spark","log":["16:23:01,151 |-INFO in ch.qos.logback.core.joran.action.AppenderAction - Naming appender as [STDOUT]","16:23:01,213 |-INFO in ch.qos.logback.core.joran.action.NestedComplexPropertyIA - Assuming default type [ch.qos.logback.access.PatternLayoutEncoder] for [encoder] property","16:23:01,368 |-INFO in ch.qos.logback.core.joran.action.AppenderRefAction - Attaching appender named [STDOUT] to null","16:23:01,368 |-INFO in ch.qos.logback.access.joran.action.ConfigurationAction - End of configuration.","16:23:01,371 |-INFO in ch.qos.logback.access.joran.JoranConfigurator@53799e55 - Registering current configuration as safe fallback point","","15/09/04 16:23:01 INFO server.ServerConnector: Started ServerConnector@388859e4{HTTP/1.1}{0.0.0.0:37394}","15/09/04 16:23:01 INFO server.Server: Started @27514ms","15/09/04 16:23:01 INFO livy.WebServer: Starting server on 37394","Starting livy-repl on http://10.0.0.11:37394"]}]}'
+    ready_sessions_json = '{"from":0,"total":1,"sessions":[{"id":0,"state":"idle","kind":"spark","log":["16:23:01,15' \
+                          '1 |-INFO in ch.qos.logback.core.joran.action.AppenderAction - Naming appender as [STDOUT]' \
+                          '","16:23:01,213 |-INFO in ch.qos.logback.core.joran.action.NestedComplexPropertyIA - As' \
+                          'suming default type [ch.qos.logback.access.PatternLayoutEncoder] for [encoder] propert' \
+                          'y","16:23:01,368 |-INFO in ch.qos.logback.core.joran.action.AppenderRefAction - Attachin' \
+                          'g appender named [STDOUT] to null","16:23:01,368 |-INFO in ch.qos.logback.access.joran.act' \
+                          'ion.ConfigurationAction - End of configuration.","16:23:01,371 |-INFO in ch.qos.logback.ac' \
+                          'cess.joran.JoranConfigurator@53799e55 - Registering current configuration as safe fallback' \
+                          ' point","","15/09/04 16:23:01 INFO server.ServerConnector: Started ServerConnector@388859' \
+                          'e4{HTTP/1.1}{0.0.0.0:37394}","15/09/04 16:23:01 INFO server.Server: Started @27514ms","' \
+                          '15/09/04 16:23:01 INFO livy.WebServer: Starting server on 37394","Starting livy-repl on' \
+                          ' http://10.0.0.11:37394"]}]}'
+    busy_sessions_json = '{"from":0,"total":1,"sessions":[{"id":0,"state":"busy","kind":"spark","log":["16:23:01,151' \
+                         ' |-INFO in ch.qos.logback.core.joran.action.AppenderAction - Naming appender as [STDOUT]",' \
+                         '"16:23:01,213 |-INFO in ch.qos.logback.core.joran.action.NestedComplexPropertyIA - Assumin' \
+                         'g default type [ch.qos.logback.access.PatternLayoutEncoder] for [encoder] property","16:23' \
+                         ':01,368 |-INFO in ch.qos.logback.core.joran.action.AppenderRefAction - Attaching appender ' \
+                         'named [STDOUT] to null","16:23:01,368 |-INFO in ch.qos.logback.access.joran.action.Configu' \
+                         'rationAction - End of configuration.","16:23:01,371 |-INFO in ch.qos.logback.access.joran.' \
+                         'JoranConfigurator@53799e55 - Registering current configuration as safe fallback point","",' \
+                         '"15/09/04 16:23:01 INFO server.ServerConnector: Started ServerConnector@388859e4{HTTP/1.1}' \
+                         '{0.0.0.0:37394}","15/09/04 16:23:01 INFO server.Server: Started @27514ms","15/09/04 16:23:' \
+                         '01 INFO livy.WebServer: Starting server on 37394","Starting livy-repl on http://10.0.0.11:' \
+                         '37394"]}]}'
     post_statement_json = '{"id":0,"state":"running","output":null}'
     running_statement_json = '{"total_statements":1,"statements":[{"id":0,"state":"running","output":null}]}'
-    ready_statement_json = '{"total_statements":1,"statements":[{"id":0,"state":"available","output":{"status":"ok","execution_count":0,"data":{"text/plain":"Pi is roughly 3.14336"}}}]}'
+    ready_statement_json = '{"total_statements":1,"statements":[{"id":0,"state":"available","output":{"status":"ok",' \
+                           '"execution_count":0,"data":{"text/plain":"Pi is roughly 3.14336"}}}]}'
     
     get_responses = []
     post_responses = []
@@ -128,6 +151,29 @@ class TestLivySession:
         http_client.get.assert_called_with("/sessions/0/statements", [200])
         assert_equals(2, http_client.get.call_count)
         assert_equals(self.pi_result, result)
+
+    def test_create_sql_context_happens_once(self):
+        kind = "spark"
+        http_client = MagicMock()
+        self.post_responses = [DummyResponse(201, self.session_create_json),
+                               DummyResponse(201, self.post_statement_json)]
+        http_client.post.side_effect = self._next_response_post
+        self.get_responses = [DummyResponse(200, self.ready_sessions_json),
+                              DummyResponse(200, self.running_statement_json),
+                              DummyResponse(200, self.ready_statement_json)]
+        http_client.get.side_effect = self._next_response_get
+        session = LivySession(http_client, kind)
+        session.start()
+
+        # Reset the mock so that post called count is accurate
+        http_client.reset_mock()
+
+        session.create_sql_context()
+        session.create_sql_context()
+
+        http_client.post.assert_called_once_with("/sessions/0/statements", [201],
+                                                 {"code": "val sqlContext = new org.apache.spark.sql.SQLContext(sc)\n"
+                                                          "import sqlContext.implicits._"})
 
     def test_create_sql_context_spark(self):
         kind = "spark"
