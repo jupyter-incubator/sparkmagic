3a73842bb45036d8dc444d48b76ff68cbfeb67f4
Alejandro Guerrero Gonzalez <alexgg@Alejandros-MacBook-Pro.local>
2015-10-10 18:23:04 -0700

    ReliableHttpClient retries on retriable status codes. Unit tested.
    Closes #5

---
+++ remotespark/livyclientlib/linearretrypolicy.py
@@ -0,0 +1,17 @@
+# Copyright (c) 2015  aggftw@gmail.com
+# Distributed under the terms of the Modified BSD License.
+
+
+class LinearRetryPolicy:
+    """Retry policy that always returns the same number of seconds to sleep between calls,
+    takes all status codes 500 or above to be retriable, and retries a given maximum number of times."""
+
+    def __init__(self, seconds_to_sleep, max_retries):
+        self._seconds_to_sleep = seconds_to_sleep
+        self._max_retries = max_retries
+
+    def should_retry(self, status_code, retry_count):
+        return status_code >= 500 and retry_count <= self._max_retries
+
+    def seconds_to_sleep(self, retry_count):
+        return self._seconds_to_sleep
--- remotespark/livyclientlib/reliablehttpclient.py
+++ remotespark/livyclientlib/reliablehttpclient.py
@@ -3,18 +3,19 @@
 
 import json
 import requests
+from time import sleep
 
 
 class ReliableHttpClient(object):
     """Http client that is reliable in its requests. Uses requests library."""
-    # TODO(aggftw): retry on retriable status codes
     # TODO(aggftw): unit tests
 
-    def __init__(self, url, headers, username, password):
+    def __init__(self, url, headers, username, password, retry_policy):
         self._url = url.rstrip("/")
         self._headers = headers
         self._username = username
         self._password = password
+        self._retry_policy = retry_policy
 
     def compose_url(self, relative_url):
         r_u = "/{}".format(relative_url.rstrip("/").lstrip("/"))
@@ -33,15 +34,20 @@ class ReliableHttpClient(object):
         return self._send_request(relative_url, accepted_status_codes, requests.delete)
 
     def _send_request(self, relative_url, accepted_status_codes, function, data=None):
-        """Sends a delete request. Returns a response."""
-        url = self.compose_url(relative_url)
+        return self._send_request_helper(self.compose_url(relative_url), accepted_status_codes, function, data, 0)
 
+    def _send_request_helper(self, url, accepted_status_codes, function, data, retry_count):
         if data is None:
             r = function(url, headers=self._headers, auth=(self._username, self._password))
         else:
             r = function(url, headers=self._headers, auth=(self._username, self._password), data=json.dumps(data))
 
-        if r.status_code not in accepted_status_codes:
-            raise ValueError("Invalid status code '{}' from {}"
-                             .format(r.status_code, url))
+        status = r.status_code
+        if status not in accepted_status_codes:
+            if self._retry_policy.should_retry(status, retry_count):
+                sleep(self._retry_policy.seconds_to_sleep(retry_count))
+                return self._send_request_helper(url, accepted_status_codes, function, data, retry_count + 1)
+            else:
+                raise ValueError("Invalid status code '{}' from {}"
+                                .format(status, url))
         return r
