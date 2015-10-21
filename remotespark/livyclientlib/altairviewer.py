af8e357ea4ae46a393bfc14a924edc6a31992ee8
Alejandro Guerrero Gonzalez <alexgg@Alejandros-MacBook-Pro.local>
2015-10-21 01:10:14 -0700

    Slight mods to Altair.
    Too tired now. Will continue tomorrow. Look at TODOs

--- remotespark/livyclientlib/altairviewer.py
+++ remotespark/livyclientlib/altairviewer.py
@@ -3,18 +3,40 @@
 import pandas as pd
 import altair.api as alt
 
+
 class AltairViewer(object):
     """A viewer that returns results as they are."""
     def visualize(self, result):
         if type(result) == pd.DataFrame:
             columns = result.columns.values
-            #v = alt.Viz(result).encode(x='buildingID', y='avg(temp_diff)')
+
+            # Simply return dataframe if only 1 column is available
+            if len(columns) <= 1:
+                return result
+
+            # Create Altair Viz
             v = alt.Viz(result)
-            # TODO: Can I inspect the type here to pick columns?!
-            # TODO: Handle no data
-            v.encode(x=columns[0], y=columns[1])
+
+            # TODO: Add helper method to Altair to know column types? Can i get them from pandas df?
+            # TODO: Select defaults - Do this on altair!: v.encode_default()
+            x_shorthand = self._get_x_shorthand(columns)
+            y_shorthand = self._get_y_shorthand(columns)
+
+            # Ideal for example would be v = alt.Viz(result).encode(x='buildingID', y='avg(temp_diff)')
+            v.encode(x=x_shorthand, y=y_shorthand)
+
+            # Configure chart
             # v.configure(width=6000, height=400, singleWidth=500, singleHeight=300)
             v.area()
+
             return v.render()
         else:
             return result
+
+    @staticmethod
+    def _get_x_shorthand(columns):
+        return columns[0]
+
+    @staticmethod
+    def _get_y_shorthand(columns):
+        return columns[1]
