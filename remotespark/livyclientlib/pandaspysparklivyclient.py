d86f6e66cd1e4480ac450163c9271268518b3a48
Ricky Stewart <rbstewart@uchicago.edu>
2015-12-21 16:29:37 -0800

    Private method begins with underscore

--- remotespark/livyclientlib/pandaspysparklivyclient.py
+++ remotespark/livyclientlib/pandaspysparklivyclient.py
@@ -24,18 +24,18 @@ class PandasPysparkLivyClient(PandasLivyClientBase):
         return records_text == "[]"
 
     def get_columns_dataframe(self, columns_text):
-        return pd.DataFrame.from_records([], columns=self.extract_strings_from_array(columns_text))
+        return pd.DataFrame.from_records([], columns=self._extract_strings_from_array(columns_text))
 
 
     def get_data_dataframe(self, records_text):
-        strings = self.extract_strings_from_array(records_text)
+        strings = self._extract_strings_from_array(records_text)
         try:
             return pd.DataFrame([json.loads(s) for s in strings])
         except ValueError:
             raise DataFrameParseException("Cannot parse object as JSON: '{}'".format(strings))
 
 
-    def extract_strings_from_array(self, s):
+    def _extract_strings_from_array(self, s):
         match = re.match(self.GET_DATA_RE, s.strip().replace('\n', ''))
         if match is not None:
             inside_brackets = match.group(1)
