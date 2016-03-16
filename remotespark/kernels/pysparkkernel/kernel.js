9e7acec5aa30475c6f4b8bfc090646e643372493
msftristew <ristew@microsoft.com>
2016-03-16 11:43:36 -0700

    Remove %%hive magic from SQL syntax highlighting

--- remotespark/kernels/pysparkkernel/kernel.js
+++ remotespark/kernels/pysparkkernel/kernel.js
@@ -1,6 +1,6 @@
 define(['base/js/namespace'], function(IPython){
         var onload = function() {
-            IPython.CodeCell.config_defaults.highlight_modes['magic_text/x-sql'] = {'reg':[/^%%sql/, /^%%hive/]};
+            IPython.CodeCell.config_defaults.highlight_modes['magic_text/x-sql'] = {'reg':[/^%%sql/]};
         }
 
         return { onload: onload }
