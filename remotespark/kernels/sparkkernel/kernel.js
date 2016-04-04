922f102829a772f2bf1e8719beddd69f27e7803a
msftristew <ristew@microsoft.com>
2016-04-04 14:59:34 -0700

    Add syntax highlighting for local mode in Scala kernel

--- remotespark/kernels/sparkkernel/kernel.js
+++ remotespark/kernels/sparkkernel/kernel.js
@@ -1,6 +1,7 @@
 define(['base/js/namespace'], function(IPython){
         var onload = function() {
             IPython.CodeCell.config_defaults.highlight_modes['magic_text/x-sql'] = {'reg':[/^%%sql/]};
+            IPython.CodeCell.config_defaults.highlight_modes['magic_text/x-python'] = {'reg':[/^%%local/]};
         }
 
         return { onload: onload }
