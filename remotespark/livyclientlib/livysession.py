64e25f1e427d484e946c4acda6510bbc308dde51
Alex Man <tc.technetium@gmail.com>
2016-05-02 16:31:14 -0700

    Fixed urls handling when appId and appInfo are missing in Livy responose. Added unit test.

--- remotespark/livyclientlib/livysession.py
+++ remotespark/livyclientlib/livysession.py
@@ -110,27 +110,18 @@ class LivySession(ObjectWithGuid):
 
     def get_app_id(self):
         if self._app_id is None:
-            session = self._http_client.get_session(self.id)
-            self._app_id = session['appId']
+            self._app_id = self._http_client.get_session(self.id).get("appId")
         return self._app_id
 
     def get_app_info(self):
-        session = self._http_client.get_session(self.id)
-        if 'appInfo' in session:
-            return session['appInfo']
-        else:
-            return {}
+        appInfo = self._http_client.get_session(self.id).get("appInfo")
+        return appInfo if appInfo is not None else {}
 
-    def get_app_info_member(self, member_name, default_value):
-        app_info = self.get_app_info()
-
-        if member_name in app_info and app_info[member_name] is not None:
-            return app_info[member_name]
-        else:
-            return default_value
+    def get_app_info_member(self, member_name):
+        return self.get_app_info().get(member_name)
 
     def get_driver_log_url(self):
-        return self.get_app_info_member("driverLogUrl", "No driver log URL is available yet.")
+        return self.get_app_info_member("driverLogUrl")
 
     def get_logs(self):
         log_array = self._http_client.get_all_session_logs(self.id)[u'log']
@@ -138,7 +129,7 @@ class LivySession(ObjectWithGuid):
         return self._logs
 
     def get_spark_ui_url(self):
-        return self.get_app_info_member("sparkUiUrl", "No Spark UI URL is available yet.")
+        return self.get_app_info_member("sparkUiUrl")
 
     @property
     def http_client(self):
