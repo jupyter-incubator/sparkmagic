b4eb1ec42aa5dbbae687fb8cc3669059cc78f1b5
Alejandro Guerrero Gonzalez <alexgg@Alejandros-MBP.T-mobile.com>
2016-05-19 02:51:24 -0700

    modified setup and requirements

--- setup.py
+++ setup.py
@@ -57,15 +57,8 @@ setup(name=NAME,
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4'],
       install_requires=[
-          'ipython>=4.0.2,<5',
-          'nose',
-          'mock',
           'requests',
-          'jupyter>=1,<2',
-          'pandas>=0.17.1',
-          'numpy',
-          'ipykernel>=4.2.2,<5',
-          'ipywidgets',
-          'plotly>=1.9.7,<2'
+          'remotesparkcommon',
+          'datawidgets'
       ])
 
