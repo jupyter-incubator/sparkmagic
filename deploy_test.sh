0619c95e67898264459afe6f933946fba3d18269
Alejandro Guerrero Gonzalez <aggftw@gmail.com>
2016-07-14 12:52:44 -0700

    only one

--- deploy_test.sh
+++ deploy_test.sh
@@ -1,9 +1,3 @@
-cd hdijupyterutils
-python setup.py sdist upload -r pypitest
-cd..
-cd autovizwidget
-python setup.py sdist upload -r pypitest
-cd ..
-cd sparkmagic
-python setup.py sdist upload -r pypitest
-cd ..
+#!/bin/sh
+set -e
+python hdijupyterutils/setup.py sdist upload -r pypitest
