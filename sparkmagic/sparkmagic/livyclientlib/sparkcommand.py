bd82dfdd121b6f660ae5f9e953cdaaed42314de5
orestis-ms <orkostak@microsoft.com>
2017-02-16 14:46:36 -0800

    fixing imports

--- sparkmagic/sparkmagic/livyclientlib/sparkcommand.py
+++ sparkmagic/sparkmagic/livyclientlib/sparkcommand.py
@@ -1,8 +1,34 @@
+import json
+import pandas as pd
+from collections import OrderedDict
+
+from sparkmagic.utils.utils import coerce_pandas_df_to_numeric_datetime
+import sparkmagic.utils.configuration as conf
+import sparkmagic.utils.constants as constants
+from sparkmagic.utils.sparkevents import SparkEvents
 from sparkmagic.livyclientlib.command import Command
+from sparkmagic.livyclientlib.exceptions import DataFrameParseException, BadUserDataException
+
+import ast
 
 class SparkStoreCommand(Command):
-    def __init__(self, code, samplemethod, maxrows, samplefraction, output_var, spark_events=None):
+    def __init__(self, code, samplemethod=None, maxrows=None, samplefraction=None, output_var=None, spark_events=None):
         super(SparkStoreCommand, self).__init__(code, spark_events)
+
+        if samplemethod is None:
+            samplemethod = conf.default_samplemethod()
+        if maxrows is None:
+            maxrows = conf.default_maxrows()
+        if samplefraction is None:
+            samplefraction = conf.default_samplefraction()
+
+        if samplemethod not in {u'take', u'sample'}:
+            raise BadUserDataException(u'samplemethod (-m) must be one of (take, sample)')
+        if not isinstance(maxrows, int):
+            raise BadUserDataException(u'maxrows (-n) must be an integer')
+        if not 0.0 <= samplefraction <= 1.0:
+            raise BadUserDataException(u'samplefraction (-r) must be a float between 0.0 and 1.0')
+
         self.samplemethod = samplemethod
         self.maxrows = maxrows
         self.samplefraction = samplefraction
@@ -16,7 +42,7 @@ class SparkStoreCommand(Command):
 
     def store_to_context(self, session):
         try:
-            command = self._to_command(session.kind, output_var)
+            command = self._to_command(session.kind, self.output_var)
             (success, records_text) = command.execute(session)
             if not success:
                 raise BadUserDataException(records_text)
@@ -30,7 +56,7 @@ class SparkStoreCommand(Command):
         if kind == constants.SESSION_KIND_PYSPARK:
             return self._pyspark_command(spark_context_variable_name)
         elif kind == constants.SESSION_KIND_PYSPARK3:
-            return self._pyspark_command(spark_context_variable_name, False)
+            return self._pyspark_command(spark_context_variable_name)
         elif kind == constants.SESSION_KIND_SPARK:
             return self._scala_command(spark_context_variable_name)
         elif kind == constants.SESSION_KIND_SPARKR:
@@ -38,23 +64,31 @@ class SparkStoreCommand(Command):
         else:
             raise BadUserDataException(u"Kind '{}' is not supported.".format(kind))
 
-    def _pyspark_command(spark_context_variable_name):
-        command = u'{}.toJSON()'.format(spark_context_variable_name)
+    def _pyspark_command(self, spark_context_variable_name, encode_result=True):
+        command = u'{}.toJSON(use_unicode=True)'.format(spark_context_variable_name)
         if self.samplemethod == u'sample':
             command = u'{}.sample(False, {})'.format(command, self.samplefraction)
         if self.maxrows >= 0:
             command = u'{}.take({})'.format(command, self.maxrows)
         else:
             command = u'{}.collect()'.format(command)
-        #what about encoding?
+        # Unicode support has improved in Python 3 so we don't need to encode.
+        if encode_result:
+            print_command = '{}.encode("{}")'.format(constants.LONG_RANDOM_VARIABLE_NAME,
+                                                     conf.pyspark_sql_encoding())
+        else:
+            print_command = constants.LONG_RANDOM_VARIABLE_NAME
+        command = u'for {} in {}: print({})'.format(constants.LONG_RANDOM_VARIABLE_NAME,
+                                                    command,
+                                                    print_command)
         return Command(command)
 
-    def _scala_command(spark_context_variable_name):
+    def _scala_command(self, spark_context_variable_name):
         #TODO add functionality
         command = u'{}.toJSON'.format(spark_context_variable_name)
         return Command(u'{}.foreach(println)'.format(command))
 
-    def _r_command(spark_context_variable_name):
+    def _r_command(self, spark_context_variable_name):
         #TODO implement
         pass
 
@@ -62,7 +96,7 @@ class SparkStoreCommand(Command):
     @staticmethod
     def _records_to_dataframe(records_text, kind):
         #TODO currently, duplicate in sqlquery.py, move both to command.py?
-        if records_text == '':
+        if records_text in ['','[]']:
             strings = []
         else:
             strings = records_text.split('\n')
--- sparkmagic/sparkmagic/magics/sparkmagicsbase.py
+++ sparkmagic/sparkmagic/magics/sparkmagicsbase.py
@@ -44,13 +44,13 @@ class SparkMagicBase(Magics):
         else:
             self.ipython_display.write(out)
             if output_var is not None:
-                spark_store_command = _spark_store_command(cell, samplemethod, maxrows, samplefraction, output_var)
+                spark_store_command = self._spark_store_command(cell, samplemethod, maxrows, samplefraction, output_var)
                 df = self.spark_controller.run_command(spark_store_command)
                 self.shell.user_ns[output_var] = df
 
     @staticmethod
-    def _spark_store_command(cell, output_var):
-        return SparkStoreCommand(cell, output_var)
+    def _spark_store_command(cell, samplemethod, maxrows, samplefraction, output_var):
+        return SparkStoreCommand(cell, samplemethod, maxrows, samplefraction, output_var)
 
     def execute_sqlquery(self, cell, samplemethod, maxrows, samplefraction,
                          session, output_var, quiet):
