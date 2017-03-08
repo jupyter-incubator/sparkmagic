7ae47ae117f8d594fcdefe31c4eb14ba0ae7d58e
Alejandro Guerrero Gonzalez <aggftw@gmail.com>
2017-03-07 16:24:15 -0800

    fix ipywidgets version requirements

--- sparkmagic/setup.py
+++ sparkmagic/setup.py
@@ -86,8 +86,8 @@ setup(name=NAME,
           'numpy',		
           'requests',
           'ipykernel>=4.2.2,<5',		
-          'ipywidgets>5.0.0',
-          'notebook>=4.2',
+          'ipywidgets>5.0.0,<7.0',
+          'notebook>=4.2,<5.0',
           'tornado>=4'
       ])
 
