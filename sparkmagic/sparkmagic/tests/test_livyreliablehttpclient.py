bdcde5fb6a297a341f76db62a3bc26c93ff6f577
Alejandro Guerrero <aggftw@gmail.com>
2017-07-27 20:50:26 -0700

    fix tests

--- sparkmagic/sparkmagic/tests/test_livyreliablehttpclient.py
+++ sparkmagic/sparkmagic/tests/test_livyreliablehttpclient.py
@@ -12,7 +12,7 @@ from sparkmagic.livyclientlib.linearretrypolicy import LinearRetryPolicy
 
 def test_post_statement():
     http_client = MagicMock()
-    livy_client = LivyReliableHttpClient(http_client)
+    livy_client = LivyReliableHttpClient(http_client, None)
     data = {"adlfj":"sadflkjsdf"}
     out = livy_client.post_statement(100, data)
     assert_equals(out, http_client.post.return_value.json.return_value)
@@ -21,7 +21,7 @@ def test_post_statement():
 
 def test_get_statement():
     http_client = MagicMock()
-    livy_client = LivyReliableHttpClient(http_client)
+    livy_client = LivyReliableHttpClient(http_client, None)
     out = livy_client.get_statement(100, 4)
     assert_equals(out, http_client.get.return_value.json.return_value)
     http_client.get.assert_called_once_with("/sessions/100/statements/4", [200])
@@ -29,7 +29,7 @@ def test_get_statement():
 
 def test_get_sessions():
     http_client = MagicMock()
-    livy_client = LivyReliableHttpClient(http_client)
+    livy_client = LivyReliableHttpClient(http_client, None)
     out = livy_client.get_sessions()
     assert_equals(out, http_client.get.return_value.json.return_value)
     http_client.get.assert_called_once_with("/sessions", [200])
@@ -37,7 +37,7 @@ def test_get_sessions():
 
 def test_post_session():
     http_client = MagicMock()
-    livy_client = LivyReliableHttpClient(http_client)
+    livy_client = LivyReliableHttpClient(http_client, None)
     properties = {"adlfj":"sadflkjsdf", 1: [2,3,4,5]}
     out = livy_client.post_session(properties)
     assert_equals(out, http_client.post.return_value.json.return_value)
@@ -46,7 +46,7 @@ def test_post_session():
 
 def test_get_session():
     http_client = MagicMock()
-    livy_client = LivyReliableHttpClient(http_client)
+    livy_client = LivyReliableHttpClient(http_client, None)
     out = livy_client.get_session(4)
     assert_equals(out, http_client.get.return_value.json.return_value)
     http_client.get.assert_called_once_with("/sessions/4", [200])
@@ -54,14 +54,14 @@ def test_get_session():
 
 def test_delete_session():
     http_client = MagicMock()
-    livy_client = LivyReliableHttpClient(http_client)
+    livy_client = LivyReliableHttpClient(http_client, None)
     livy_client.delete_session(99)
     http_client.delete.assert_called_once_with("/sessions/99", [200, 404])
 
 
 def test_get_all_session_logs():
     http_client = MagicMock()
-    livy_client = LivyReliableHttpClient(http_client)
+    livy_client = LivyReliableHttpClient(http_client, None)
     out = livy_client.get_all_session_logs(42)
     assert_equals(out, http_client.get.return_value.json.return_value)
     http_client.get.assert_called_once_with("/sessions/42/log?from=0", [200])
