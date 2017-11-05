c70cf2d60e0d33d06320a53073250e9495adc6fc
Tomek <terr@onet.eu>
2017-11-05 18:36:31 +0100

    ISSUE#413 - remove accidental whitespace

--- sparkmagic/sparkmagic/livyclientlib/sendstringtosparkcommand.py
+++ sparkmagic/sparkmagic/livyclientlib/sendstringtosparkcommand.py
@@ -19,7 +19,7 @@ class SendStringToSparkCommand(SendToSparkCommand):
     def _r_command(self, input_variable_name, input_variable_value, output_variable_name):
         self._assert_input_is_string_type(input_variable_name, input_variable_value)
         escaped_input_variable_value = re.escape(input_variable_value)
-        code_to_execute = u'assign("{}","{}")'.format(output_variable_  name, escaped_input_variable_value)
+        code_to_execute = u'assign("{}","{}")'.format(output_variable_name, escaped_input_variable_value)
         return Command(code_to_execute)
 
     def _assert_input_is_string_type(self, input_variable_name, input_variable_value):
