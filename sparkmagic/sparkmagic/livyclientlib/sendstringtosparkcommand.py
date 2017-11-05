e3dca75184cb76b09f0556bef981dc48bf42090f
Tomek <terr@onet.eu>
2017-11-05 18:31:38 +0100

    ISSUE#413 - escape R string from special characters even more correctly

--- sparkmagic/sparkmagic/livyclientlib/sendstringtosparkcommand.py
+++ sparkmagic/sparkmagic/livyclientlib/sendstringtosparkcommand.py
@@ -2,6 +2,7 @@ from sparkmagic.livyclientlib.sendtosparkcommand import SendToSparkCommand
 from sparkmagic.livyclientlib.command import Command
 from sparkmagic.livyclientlib.exceptions import BadUserDataException
 
+import re
 
 class SendStringToSparkCommand(SendToSparkCommand):
 
@@ -17,7 +18,8 @@ class SendStringToSparkCommand(SendToSparkCommand):
 
     def _r_command(self, input_variable_name, input_variable_value, output_variable_name):
         self._assert_input_is_string_type(input_variable_name, input_variable_value)
-        code_to_execute = u'assign(\'"{}"\',\'"{}"\')'.format(output_variable_name, input_variable_value)
+        escaped_input_variable_value = re.escape(input_variable_value)
+        code_to_execute = u'assign("{}","{}")'.format(output_variable_  name, escaped_input_variable_value)
         return Command(code_to_execute)
 
     def _assert_input_is_string_type(self, input_variable_name, input_variable_value):
