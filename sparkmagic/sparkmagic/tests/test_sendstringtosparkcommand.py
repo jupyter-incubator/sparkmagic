febc8a029b8218f42aba6c7b25361270d09ab68e
Tomek <megatrontomaszdudek@gmail.com>
2017-12-10 12:25:05 +0100

    ISSUE#413 - fix tests

--- sparkmagic/sparkmagic/tests/test_sendstringtosparkcommand.py
+++ sparkmagic/sparkmagic/tests/test_sendstringtosparkcommand.py
@@ -18,32 +18,34 @@ def test_send_to_r():
     input_variable_value = "value"
     output_variable_name = "output"
     sparkcommand = SendStringToSparkCommand(input_variable_name, input_variable_value, output_variable_name)
-    sparkcommand._scala_command = MagicMock(return_value=MagicMock())
+    sparkcommand._r_command = MagicMock(return_value=MagicMock())
     sparkcommand.to_command("sparkr", input_variable_name, input_variable_value, output_variable_name)
-    sparkcommand._scala_command.assert_called_with(input_variable_name, input_variable_value, output_variable_name)
+    sparkcommand._r_command.assert_called_with(input_variable_name, input_variable_value, output_variable_name)
 
 def test_send_to_pyspark():
     input_variable_name = "input"
     input_variable_value = "value"
     output_variable_name = "output"
     sparkcommand = SendStringToSparkCommand(input_variable_name, input_variable_value, output_variable_name)
-    sparkcommand._scala_command = MagicMock(return_value=MagicMock())
+    sparkcommand._pyspark_command = MagicMock(return_value=MagicMock())
     sparkcommand.to_command("pyspark", input_variable_name, input_variable_value, output_variable_name)
-    sparkcommand._scala_command.assert_called_with(input_variable_name, input_variable_value, output_variable_name)
+    sparkcommand._pyspark_command.assert_called_with(input_variable_name, input_variable_value, output_variable_name)
 
 def test_send_to_pyspark3():
     input_variable_name = "input"
     input_variable_value = "value"
     output_variable_name = "output"
     sparkcommand = SendStringToSparkCommand(input_variable_name, input_variable_value, output_variable_name)
-    sparkcommand._scala_command = MagicMock(return_value=MagicMock())
+    sparkcommand._pyspark_command = MagicMock(return_value=MagicMock())
     sparkcommand.to_command("pyspark3", input_variable_name, input_variable_value, output_variable_name)
-    sparkcommand._scala_command.assert_called_with(input_variable_name, input_variable_value, output_variable_name)
+    sparkcommand._pyspark_command.assert_called_with(input_variable_name, input_variable_value, output_variable_name)
 
 def test_to_command_invalid():
-    variable_name = "var_name"
+    input_variable_name = "input"
+    input_variable_value = 42
+    output_variable_name = "output"
     sparkcommand = SendStringToSparkCommand(input_variable_name, input_variable_value, output_variable_name)
-    assert_raises(BadUserDataException, sparkcommand.to_command, "invalid", variable_name)
+    assert_raises(BadUserDataException, sparkcommand.to_command, "invalid", input_variable_name, input_variable_value, output_variable_name)
 
 def test_should_raise_when_input_aint_a_string():
     input_variable_name = "input"
@@ -57,7 +59,7 @@ def test_should_create_a_valid_scala_expression():
     input_variable_value = "value"
     output_variable_name = "output"
     sparkcommand = SendStringToSparkCommand(input_variable_name, input_variable_value, output_variable_name)
-    assert_equals(sparkcommand._scala_command(variable_name),
+    assert_equals(sparkcommand._scala_command(input_variable_name, input_variable_value, output_variable_name),
                     Command(u'var {} = """{}"""').format(output_variable_name, input_variable_value))
 
 def test_should_create_a_valid_python_expression():
@@ -65,13 +67,13 @@ def test_should_create_a_valid_python_expression():
     input_variable_value = "value"
     output_variable_name = "output"
     sparkcommand = SendStringToSparkCommand(input_variable_name, input_variable_value, output_variable_name)
-    assert_equals(sparkcommand._pyspark_command(variable_name),
+    assert_equals(sparkcommand._pyspark_command(input_variable_name, input_variable_value, output_variable_name),
                   Command(u'{} = \'\'\'{}\'\'\'').format(output_variable_name, input_variable_value))
 
-def test_should_create_a_valid_scala_expression():
+def test_should_create_a_valid_r_expression():
     input_variable_name = "input"
     input_variable_value = "value"
     output_variable_name = "output"
     sparkcommand = SendStringToSparkCommand(input_variable_name, input_variable_value, output_variable_name)
-    assert_equals(sparkcommand._r_command(variable_name),
+    assert_equals(sparkcommand._r_command(input_variable_name, input_variable_value, output_variable_name),
                   Command(u'''assign("{}","{}")''').format(output_variable_name, input_variable_value))
