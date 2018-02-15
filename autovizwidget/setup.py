4f7222c755fd500afd95980b22e03453e10c6dea
Thomas Baldwin <tkb@sent.com>
2018-02-14 21:36:23 -0800

    Update minimum plotly dependency to 1.13.0
    To support the connected=True argument to init_noteboook_mode.

--- autovizwidget/setup.py
+++ autovizwidget/setup.py
@@ -57,7 +57,7 @@ setup(name=NAME,
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4'],
       install_requires=[
-          'plotly>=1.10.0,<3.0',
+          'plotly>=1.13.0,<3.0',
           'ipywidgets>5.0.0,<8.0',
           'hdijupyterutils>=0.6',
           'notebook>=4.2,<6.0',
