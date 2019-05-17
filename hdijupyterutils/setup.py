16d3c705bcb6f765cdfc46e7db4ead267898532b
juliusvonkohout <45896133+juliusvonkohout@users.noreply.github.com>
2019-05-17 17:47:31 +0200

    Update setup.py

--- hdijupyterutils/setup.py
+++ hdijupyterutils/setup.py
@@ -52,7 +52,6 @@ setup(name=NAME,
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'],
       install_requires=[
