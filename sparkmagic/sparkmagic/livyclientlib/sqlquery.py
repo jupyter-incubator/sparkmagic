9dd3252a7468bba32dd3581f277da97c0dd20bcf
stczwd <qcsd2011@163.com>
2019-10-12 15:22:43 +0800

    SQLQuery default print error instead of throwing error stack

--- sparkmagic/sparkmagic/livyclientlib/sqlquery.py
+++ sparkmagic/sparkmagic/livyclientlib/sqlquery.py
@@ -5,7 +5,7 @@ import sparkmagic.utils.configuration as conf
 import sparkmagic.utils.constants as constants
 from sparkmagic.utils.sparkevents import SparkEvents
 from .command import Command
-from .exceptions import DataFrameParseException, BadUserDataException
+from .exceptions import DataFrameParseException, BadUserDataException, SparkStatementException
 
 
 class SQLQuery(ObjectWithGuid):
@@ -49,13 +49,19 @@ class SQLQuery(ObjectWithGuid):
         self._spark_events.emit_sql_execution_start_event(session.guid, session.kind, session.id, self.guid,
                                                           self.samplemethod, self.maxrows, self.samplefraction)
         command_guid = ''
+        result = None
         try:
             command = self.to_command(session.kind, session.sql_context_variable_name)
             command_guid = command.guid
             (success, records_text, mimetype) = command.execute(session)
             if not success:
-                raise BadUserDataException(records_text)
-            result = records_to_dataframe(records_text, session.kind, self._coerce)
+                if conf.spark_statement_errors_are_fatal():
+                    if conf.shutdown_session_on_spark_statement_errors():
+                        self.spark_controller.cleanup()
+                    raise SparkStatementException(records_text)
+                self.ipython_display.send_error(records_text)
+            else:
+                result = records_to_dataframe(records_text, session.kind, self._coerce)
         except Exception as e:
             self._spark_events.emit_sql_execution_end_event(session.guid, session.kind, session.id, self.guid,
                                                             command_guid, False, e.__class__.__name__, str(e))
