2db3885dd9df884d6ecea7a78fe67d86d96fc9b4
Gaspard Blanchet <gaspard.blanchet@datadoghq.com>
2021-09-09 18:47:48 +0200

    Add comma to setup file

--- hdijupyterutils/setup.py
+++ hdijupyterutils/setup.py
@@ -61,7 +61,7 @@ setup(
         "nose",
         "mock",
         "ipywidgets>5.0.0",
-        "ipykernel>=4.2.2<6.0.0",
+        "ipykernel>=4.2.2,<6.0.0",
         "jupyter>=1",
         "pandas>=0.17.1",
         "numpy",
